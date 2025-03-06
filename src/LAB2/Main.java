6ab4a3b4020928738fb3879c332cc85f87f8fd0f
Student <student@b623-10.intranet.cs.upt.ro>
2025-03-06 13:23:30 +0200

    adaugat class Ajutor si Main

--- src/LAB2/Main.java
+++ src/LAB2/Main.java
@@ -1,10 +1,23 @@
 package LAB2;
 
+import java.util.Scanner;
 
 public class Main {
+	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+		Ajutor instanta = new Ajutor();
+		System.out.println("Introdu text: ");
+		String mesaj = scan.next();
+		System.out.println(mesaj+"\n");
+		if(mesaj.equals("hello")) {
+			instanta.Afisare();
+		}else if(mesaj.equals("exit")){
+			System.out.println("PRogramul a iesit di executie");
+			System.exit(0);
+		}else {
+			instanta.Info();
+		}
 
 	}
 
